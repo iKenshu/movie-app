758889361ab583550fb60c2cd06674d8c8497e97
iKenshu <undefined.hlo.o@gmail.com>
2018-03-06 08:37:56 -0400

    adding some test

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Movie
+
+
+class MovieModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        test_user = User.objects.create(username='ikenshu', password='12345')
+        test_user.save()
+        Movie.objects.create(
+            title='Get Out',
+            synopsis='wow movie',
+            slug='get-out',
+            created_by=test_user
+        )
+
+    def test_title_max_lenght(self):
+        movie = Movie.objects.get(id=1)
+        max_length = movie._meta.get_field('title').max_length
+        self.assertEquals(max_length, 140)
+
+    def test_slug(self):
+        movie = Movie.objects.get(id=1)
+        self.assertEquals(movie.slug, 'get-out')
