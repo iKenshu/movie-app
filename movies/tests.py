eb85ca0c561d7aaf1a5b989b22d255922d00c1c5
iKenshu <undefined.hlo.o@gmail.com>
2018-10-08 06:40:07 -0400

    updating django

--- movies/tests.py
+++ movies/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+from django.core.files.uploadedfile import SimpleUploadFile
 
 from .models import Movie
+from .views import MovieList
 
 
 class MovieModelTest(TestCase):
@@ -25,3 +29,30 @@ class MovieModelTest(TestCase):
     def test_slug(self):
         movie = Movie.objects.get(id=1)
         self.assertEquals(movie.slug, 'get-out')
+
+
+class MovieViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        test_user = User.objects.create(username='ikenshu', password='12345')
+        test_user.save()
+        Movie.objects.create(
+            title='Get Out',
+            synopsis='wow movie',
+            slug='get-out',
+            created_by=test_user
+        )
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_url_accesible_by_name(self):
+        response = self.client.get(reverse('Movie:list'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('Movie:list'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUser(response, 'movies/movie_list.html')
